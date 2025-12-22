4149f6119c1979c2382dafc51564cfa557e5ec17
Elias Nowak <e12407768@student.tuwien.ac.at>
2025-12-22 19:19:22 +0100

    add header

--- src/business/include/state.h
+++ src/business/include/state.h
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2025 CareLoop Labs
+
 /*
  * State machine public interface (business layer only).
  * https://docs.zephyrproject.org/latest/services/smf/index.html#state-creation 
